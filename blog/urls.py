4c454edcd570ee8a272b96053e8269561957c6cd
silverXraven <SilverRivers@yandex.ru>
2020-01-27 23:07:29 +0500

    добавлено вид и шаблон для детализации поста в блоге а немного css

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 """views.post_list — это правильное направление для запроса к твоему веб-сайту по адресу 'http://127.0.0.1:8000/'.
 name='post_list' — это имя URL, которое будет использовано, чтобы идентифицировать его"""
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 #Точка перед models означает текущую директорию или текущее приложение. Поскольку views.py и models.py находятся в одной директории, мы можем использовать точку . и имя файла (без расширения .py). Затем мы импортируем модель (Post).
 # Create your views here.
@@ -8,3 +9,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')#Нам осталось передать QuerySet posts в шаблон
     return render(request, 'blog/post_list.html',{'posts': posts})#это место, куда мы можем добавить что-нибудь для использования в шаблоне. )
 #Как ты можешь заметить, мы создали функцию (def) с именем post_list, которая принимает request в качестве аргумента и возвращает (return) результат работы функции render, которая уже соберёт наш шаблон страницы blog/post_list.html.
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
