4484264daea1df9431cb2b34975a6b2a5b6ffd1e
silverXraven <SilverRivers@yandex.ru>
2020-02-15 01:08:16 +0500

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 """views.post_list — это правильное направление для запроса к твоему веб-сайту по адресу 'http://127.0.0.1:8000/'.
 name='post_list' — это имя URL, которое будет использовано, чтобы идентифицировать его"""
